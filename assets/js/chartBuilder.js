2c0271aaf43dfb423ae84ae80407d53902356ea8
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-04-02 07:56:31 +0530

    Chart Building Script add to another js file

---
+++ assets/js/chartBuilder.js
@@ -0,0 +1,36 @@
+function showGraph() {
+    {
+        $.post("data.php ",
+            function(data) {
+                var date = [];
+                var cases = [];
+                var i = 0;
+                var dataVari = JSON.parse(data);
+                for (var i in dataVari) {
+                    date.push(dataVari[i].date.split(" ")[0]);
+                    cases.push(dataVari[i].newCases);
+                }
+
+                var chartdata = {
+                    labels: date,
+                    datasets: [{
+                        label: 'new Cases',
+                        backgroundColor: 'red',
+                        borderColor: 'red',
+                        hoverBackgroundColor: '#CCCCCC',
+                        hoverBorderColor: '#666666',
+                        fill: false,
+                        steppedLine: false,
+                        data: cases
+                    }]
+                };
+
+                var graphTarget = $("#graphCanvas ");
+
+                var barGraph = new Chart(graphTarget, {
+                    type: 'line',
+                    data: chartdata,
+                });
+            });
+    }
+}
