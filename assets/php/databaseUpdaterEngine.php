b4e7d105a66339a34dcbdc013d0c2e2221ff9848
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-04-05 14:48:32 +0530

    Database data sync algorithm updated.

--- assets/php/databaseUpdaterEngine.php
+++ assets/php/databaseUpdaterEngine.php
@@ -6,33 +6,60 @@ if(mysqli_connect_error($conn)){
     $sqlGetCount="SELECT COUNT(`date`) FROM `dailyupdate`;";
     $resultSetGetCount=mysqli_query($conn,$sqlGetCount);
     $count=mysqli_fetch_assoc($resultSetGetCount)["COUNT(`date`)"];
-    $sqlQuery = "SELECT  `date`,`newCases`  FROM `dailyupdate` WHERE `tableIndex`=$count;";
+    $sqlQuery = "SELECT  *  FROM `dailyupdate` WHERE `tableIndex`=$count;";
     $result = mysqli_query($conn,$sqlQuery);
     $row=mysqli_fetch_assoc($result);
     $lastUpdatedDB=$row['date'];
     $newCasesDB=$row['newCases'];
+    $activeCasesDB=$row['currentActiveCases'];
+    $totleCasesDB=$row['totalCases'];
     // echo json_encode($row);
     $sqlQueryDU="";
+    $key=0;
     if($_GET['action']=='check'){
         $timeStamp=$_GET['timeStamp'];
         $newCases=$_GET['newCases'];
         $totleCases=$_GET['totleCases'];
-        if((explode(" ",$timeStamp)[0])==$lastUpdatedDB){
-            if($newCasesDB==$newCases){
-                //Matched.No action needed.
-            }else{
-                //Today, data set should update.
-                $sqlQueryDU="UPDATE `dailyupdate` SET `newCases`=$newCases,`totalCases`=$totleCases WHERE `date`='$timeStamp';";
+        $activeCases=$_GET['activeCases'];
+        // echo ($timeStamp);
+        // echo ($lastUpdatedDB);
+        if($timeStamp==$lastUpdatedDB){
+            if($newCasesDB!=$newCases){
+                $sqlQueryDU="UPDATE `dailyupdate` SET `newCases`=$newCases WHERE `date`='$timeStamp';";
+                $result=mysqli_query($conn,$sqlQueryDU);
+                if($result){
+                    $key=$key+1;
+                }
             }
+            // echo($sqlQueryDU);
+            if($activeCasesDB!=$activeCases){
+                $sqlQueryDU="UPDATE `dailyupdate` SET `currentActiveCases`=$activeCases WHERE `date`='$timeStamp';";
+                $result=mysqli_query($conn,$sqlQueryDU);
+                if($result){
+                    $key=$key+1;
+                }
+            }
+            // echo($sqlQueryDU);
+            // echo($totleCasesDB);
+            // echo($totleCases);
+            if($totleCasesDB!=$totleCases){
+                $sqlQueryDU="UPDATE `dailyupdate` SET `totalCases`=$totleCases WHERE `date`='$timeStamp';";
+                $result=mysqli_query($conn,$sqlQueryDU);
+                if($result){
+                    $key=$key+1;
+                }
+            }
+            // echo($sqlQueryDU);
         }else{
-            //Need to create a new entry
-            $sqlQueryDU="INSERT INTO `dailyupdate`(`newCases`, `date`,`totalCases`) VALUES ($newCases,'$timeStamp',$totleCases);";   
-        }
-        if($sqlQueryDU!=""){
+            $sqlQueryDU="INSERT INTO `dailyupdate`(`newCases`, `date`,`totalCases`,`currentActiveCases`) VALUES ($newCases,'$timeStamp',$totleCases,$activeCases);";   
             $result=mysqli_query($conn,$sqlQueryDU);
-            if($result){
-                $retData = array('queryState' => 'Sucess', );
-            }
+                if($result){
+                    $key=$key+1;
+                }
+                // echo($sqlQueryDU);
+        }
+        if($key>0){
+            $retData = array('queryState' => 'Sucess', );
         }else{
             $retData = array('queryState' => 'Fail', );
         }
